2f8d8cb29d593fcb00b617f3e9e60c07972edfe3
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-09 20:44:36 +0900

    feat: board controller 추가

--- nbcamp/sparta_nest/src/board/board.controller.ts
+++ nbcamp/sparta_nest/src/board/board.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BoardService } from './board.service';
+import { CreateArticleDto } from './create-article.dto';
+import { DeleteArticleDto } from './delete-article.dto';
+import { UpdateArticleDto } from './update-article.dto';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  // 서비스 주입
+  constructor(private readonly boardService: BoardService) {}
+
+  // 게시물 목록을 가져오는 API
+  @Get('/articles')
+  getArticles() {
+    return this.boardService.getArticles();
+  }
+
+  // 게시물 상세보기 -> 게시물 ID로 확인
+  @Get('/articles/:id')
+  getArticleById(@Param('id') articleId: number) {
+    return this.boardService.getArticleById(articleId);
+  }
+
+  // 게시물 작성
+  @Post('/articles')
+  createArticle(@Body() data: CreateArticleDto) {
+    return this.boardService.createArticle(
+      data.title,
+      data.content,
+      data.password,
+    );
+  }
+
+  // 게시물 수정
+  @Put('/articles/:id')
+  updateArticle(
+    @Param('id') articleId: number,
+    @Body() data: UpdateArticleDto,
+  ) {
+    return this.boardService.updateArticle(
+      articleId,
+      data.title,
+      data.content,
+      data.password,
+    );
+  }
+
+  // 게시물 삭제
+  @Delete('/articles/:id')
+  deleteArticle(
+    @Param('id') articleId: number,
+    @Body() data: DeleteArticleDto,
+  ) {
+    return this.boardService.deleteArticle(articleId, data.password);
+  }
+}
