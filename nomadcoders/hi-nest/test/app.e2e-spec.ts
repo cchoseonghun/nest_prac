a9e1fc1d845fff5445a8d68acb4be787b15bf5be
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-02-15 00:35:55 +0900

    nomadcoders: #4 E2E TESTING - COMPLETED

--- nomadcoders/hi-nest/test/app.e2e-spec.ts
+++ nomadcoders/hi-nest/test/app.e2e-spec.ts
@@ -1,17 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeEach(async () => {
+  beforeAll(async () => {
+    // beforeEach 사용 시 새로운 테스트를 진행할 때마다
+    // 어플리케이션이 새로 생성되기 때문에 그걸 막기 위해 beforeAll로 변경
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // req.query에 대해 확인해보면 class-transformer가 적용되지 않는걸 알 수 있는데
+    // main.ts에서 처럼 작동하도록 설정해줘야 한다. 아래처럼
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +31,58 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
+      // localhost:3000 안쓰기 위해 app.getHttpServer() 시용
+    });
+    it('POST 201', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE 404', () => {
+      return request(app.getHttpServer()).delete('/movies').expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    // it.todo('GET');
+    // it.todo('DELETE');
+    // it.todo('PATCH');
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
   });
 });
